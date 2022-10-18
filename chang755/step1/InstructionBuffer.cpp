7f4471cb19192fadc2fe0d6926d5b2f4b99e3c12
SeanoChang <89418312+SeanoChang@users.noreply.github.com>
2022-10-17 21:47:53 -0400

    -v0.0.1_planning-out-step1-files

--- chang755/step1/InstructionBuffer.cpp
+++ chang755/step1/InstructionBuffer.cpp
@@ -2,3 +2,7 @@
 The class for constructing the instruction table structure.
 Basically, it is the stack to store the op instructions.
 */
+#include <iostream>
+
+#include "InstructionBuffer.h"
+
---
+++ chang755/step1/InstructionBuffer.h
@@ -0,0 +1,14 @@
+#ifndef INSTRUCTIONBUFFER_H
+#define INSTRUCTIONBUFFER_H
+
+#include <iostream>
+
+class InstructionBuffer {
+    public:
+        InstructionBuffer();
+        ~InstructionBuffer();
+    //private: 
+    // pointer to opcode objects
+};
+
+#endif
