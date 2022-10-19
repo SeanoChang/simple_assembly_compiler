7f1e2a22648f6cc70dc910197a0c1a8769cb8c35
Josh9281 <89463394+Josh9281@users.noreply.github.com>
2022-10-19 15:51:52 -0400

    Update Parser.cpp

--- chang755/step1/Parser.cpp
+++ chang755/step1/Parser.cpp
@@ -2,3 +2,49 @@
 The parser.cpp is where all the parsing happens. 
 It is the main class that calls all the other classes.
 */
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include "Stmt.h"
+using namespace std;
+
+std::vector<std::string> stmt_ops
+{ 
+  "start",
+  "end",
+  "exit",
+  "return",
+  "pop",
+  "dup",
+  "swap",
+  "add",
+  "negate",
+  "mul",
+  "div",
+  "printtos",
+  "prints"                          
+};
+
+std::vector<std::string> var_stmt_ops
+{
+  "declscal",
+  "pushscal",
+  "pusharr",
+  "popscal",
+  "poparr"
+};
+
+std::vector<std::string> int_stmt_ops{"pushi"};
+
+std::vector<std::string> varLen_stmt_ops{"declarr"};
+
+std::vector<std::string> label_stmt_ops
+{
+  "label",
+  "gosublabel",
+  "jump",
+  "jumpzero",
+  "jumpnzero",
+  "gosub"
+};
