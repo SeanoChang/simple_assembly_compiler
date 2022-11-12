e1ae07c2b8dd3f9e7b357fd2751981919b19afb7
SeanoChang <89418312+SeanoChang@users.noreply.github.com>
2022-11-12 16:21:31 -0500

    step2 initial

---
+++ chang755/step2/ReadWriteFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "ReadWriteFile.h"
+
+/*
+This file will be handling the file i/o.
+*/
+
+// Read the instructions from the file and store them in the instruction memory.
+// First, for cases that have print statements, we will need to read the strings from the file first,
+// and store them in the string table.
+// Then, we read the instructions and check if the instructions is valid.
+// Only if the instruction is valid that we will store it in the memory.
+// Otherwise we return null.
+
+
+
+// Write output
+// For print statements, we fetch the string from the string table and print it out.
+// For other instructions, 
---
+++ chang755/step2/step2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+This is the main of step two.
+For this step, we will be building the Virtual Machine.
+
+*/
+
+int main(int argc, char* argv[]) {
+    // Read the instructions from the file
+    // and store them in the instruction memory,
+    // string table if needed.
+
+
+    // Execute the instructions in the instruction memory
+    
+
+}
